6338d8274fd29d70c45044afe3bfeede3fb9e705
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-22 00:04:15 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import Head from 'next/head';
 import { QueticoSimulator } from '../components/QueticoSimulator';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Quetico 3PL Operations Simulator</title>
-        <meta name="description" content="Experience 3PL operations with Quetico's interactive simulator" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <QueticoSimulator />
-      </main>
-    </div>
-  );
+  return <QueticoSimulator />;
 }
